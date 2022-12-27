78d3115ee1152746562937da917c39933c378987
Santiago Caballero <saantyc12.caballero@gmail.com>
2022-12-27 15:58:42 -0300

    test

---
+++ src/database/connection.ts
@@ -0,0 +1,7 @@
+//Here we are going to connect with the database on mongo using mongoose!
+const connect = () => {
+    return console.log('The DB is connected!')
+}
+
+
+module.exports = connect
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 const express = require('express')
-
 const app = express()
+const connection = require('./database/connection.ts')
 
+connection()
 app.listen(3001, () => console.log('Server run on port ', 3001))
